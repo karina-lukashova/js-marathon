14176657357eab2dcdd8e6bc087acf83b7b500fa
Karina Lukashova <karina-luka@yandex.ru>
2020-08-20 21:12:10 +0300

    Оптимизировала функцию

--- main.js
+++ main.js
@@ -7,17 +7,15 @@ let result = '';
 function getRow(firstRow, secondRow) {
   for (let i = 0; i < firstRow.length; i++) {
     if (firstRow.charAt(i) === 'а') {
-      firstCount++} else {
-        firstCount = firstCount}}
+      firstCount++}}
   for (let i = 0; i < secondRow.length; i++) {
     if (secondRow.charAt(i) === 'а') {
-      secondCount++} else {
-        secondCount = secondCount}}
+      secondCount++}}
   if (firstCount > secondCount) {
     result = firstRow} else if (firstCount < secondCount) {
       result = secondRow} else {
         result = 'Amount of a is equal'}
-  alert(result);
+  return(result);
 }
 
 alert(getRow(firstRow, secondRow));
